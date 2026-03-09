a092784623f59915ab100fac68ccc4727c17162e
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-03-09 06:11:07 +0530

    i will work on it later

--- mini-projects/Kanban Board/script.js
+++ mini-projects/Kanban Board/script.js
@@ -6,166 +6,77 @@ const todos=document.getElementById("todo");
 const doing=document.getElementById("doing");
 const done=document.getElementById("done");
 const appState=[];
-
+const todostatus=null;
 function addtasks(event){
     const target=event.target;
     if(target.matches("#addTaskBtn")){
         if(taskInput.value==="")return;
-        appState.push({taskTitle:taskInput.value,id: Date.now(),completed:false,status:"todo"})
+        appState.push({taskTitle:taskInput.value,id: Date.now(),completed:false,})
         console.log(appState);
         taskInput.value="";
-        render();
+        todorender();
     }
  }
- function render(){
-    taskContainer.innerHTML="";
+
+ function kanbanBoard(event){
+     const target=event.target;
+     if(target.matches(".move-btn")){  
+          doingrender();
+     }
+     if(target.matches(".delete-btn")){
+        const li=target.closest("li");
+        const id=Number(li.dataset.id);
+        deletedata(id);
+     }
+ };
+ function deletedata(id){
     for(let i=0;i<appState.length;i++){
-        if(appState[i].status==="todo"){
-
-for(let todo of appState){
-
-    const li=document.createElement("li");
-
-    li.classList.add("task-card");
-
-    li.dataset.id=todo.id;
-
-   li.innerHTML=`
-    <div class="task-text">
-        ${todo.taskTitle}
-    </div>
-
-    <div class="task-actions">
-        <button class="done-btn">→ Done</button>
-        <button class="delete-btn">✕</button>
-    </div>
-`;
-
-    todos.append(li);
-
-}
+        if(appState[i].id===id){
+            appState.splice(i,1);
+            break;
         }
-        if(appState[i].status==="doing"){
-
-for(let todo of appState){
-
-    const li=document.createElement("li");
-
-    li.classList.add("task-card");
-
-    li.dataset.id=todo.id;
-
-   li.innerHTML=`
-    <div class="task-text">
-        ${todo.taskTitle}
-    </div>
-
-    <div class="task-actions">
-        <button class="delete-btn">✕</button>
-    </div>
-`;
-
-    doing.append(li);
-        }
-        if(appState[i].status==="done"){
-
-for(let todo of appState){
-
-    const li=document.createElement("li");
-
-    li.classList.add("task-card");
-
-    li.dataset.id=todo.id;
-
-   li.innerHTML=`
-    <div class="task-text">
-        ${todo.taskTitle}
-    </div>
-
-    <div class="task-actions">
-        <button class="delete-btn">✕</button>
-    </div>
-`;
-
-    done.append(li);
-}
-        }
+        todorender();
+        doingrender();
     }
  }
-}
+ function doingrender(){
+    doing.innerHTML="";
+    for(let todo of appState){ 
+    const li=document.createElement("li");
+     li.classList.add("task-card");
+    li.dataset.id=todo.id;
+    li.innerHTML=`
+     <div class="task-text">
+         ${todo.taskTitle}
+     </div>
+     <div class="task-actions">
+        <button class="move-btn">→ Done</button>
+         <button class="delete-btn">✕</button>
+     </div>
+ `;
 
-function kanbanBoard(event){
-    const target=event.target;
-    if(target.matches(".Doing-btn")){
-        appState.status="doing";
-        console.log(appState.status);
-    };
-     if(target.matches(".done-btn")){
-        appState.status="Done";
-        console.log(appState.status);
-    };
-    
-}
+     doing.append(li);
+    }
+ }
+ function todorender(){
+   todos.innerHTML="";
+ for(let todo of appState){ 
+    const li=document.createElement("li");
+     li.classList.add("task-card");
+    li.dataset.id=todo.id;
+    li.innerHTML=`
+     <div class="task-text">
+         ${todo.taskTitle}
+     </div>
+     <div class="task-actions">
+        <button class="move-btn">→ Doing</button>
+         <button class="delete-btn">✕</button>
+     </div>
+ `;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function kanbanBoard(event){
-//     const target=event.target;
-//     if(target.matches(".move-btn")){
-//         appState.status="todo";    
-//         // doingRender();
-//     }
-// };
-// function render(){
-// todos.innerHTML="";
-
-// for(let todo of appState){
-
-//     const li=document.createElement("li");
-
-//     li.classList.add("task-card");
-
-//     li.dataset.id=todo.id;
-
-//    li.innerHTML=`
-//     <div class="task-text">
-//         ${todo.taskTitle}
-//     </div>
-
-//     <div class="task-actions">
-//         <button class="move-btn">→ Doing</button>
-//         <button class="delete-btn">✕</button>
-//     </div>
-// `;
-
-//     todos.append(li);
-// }
-// }
+     todos.append(li);
+    }
+ }
+ 
 board.addEventListener("click",kanbanBoard);
 header.addEventListener("click",addtasks);
